c18a1af5af3939c6294a6665bb80b4ecf5933352
Cristian Doring Molon <cristiandoring@gmail.com>
2024-01-12 15:11:01 -0300

    Commit

--- 1071/script.py
+++ 1071/script.py
@@ -2,10 +2,13 @@ X = int(input(''))
 Y = int(input(''))
 
 soma = 0
-
-X = abs(X)
-Y = abs(Y)
-
+if X > 0 and Y < 0:
+    X = abs(X)
+    Y = abs(Y)
+elif X < 0 and Y > 0:
+    X = abs(X)
+    Y = abs(Y)
+    
 if X > Y:
     for i in range(Y,X):
             if i % 2 == 1:
@@ -14,5 +17,5 @@ elif X < Y:
     for i in range(X,Y):
            if i % 2 == 1:
                soma += i
-     
+    
 print(soma)
